755d9427781022792fb1bb0e430cad33d40dcacc
Diego <msnimpu@gmail.com>
2020-02-05 13:14:50 +0300

    Метод Controller::requestExecute реализует безопасную связку Controller::test и Controller::execute

--- src/main/java/me/ling/kipfin/vkbot/commands/Controller.java
+++ src/main/java/me/ling/kipfin/vkbot/commands/Controller.java
@@ -13,18 +13,29 @@ public abstract class Controller {
     /**
      * Тестирует контрллер
      *
+     * Данный метод является главным в контроллере - он отвечает за роутинг. Если данный метод возвращает
+     * значение true, будет запущен метод Controller::execute данного класса.
+     *
+     * @warning метод `Controller::execute` данного класса НИКОГДА не будет запущен, кроме случаев, когда
+     *   `Controller::test` возвращает true. <b>Нет необходимости делнть двойные проверки</b>!
+     *
      * @param text - полученный текст
      * @param user - пользователь
-     * @param args - дополнительные аргументы
+     * @param args - аргументы
      * @return - результат проверки
      */
     public abstract boolean test(String text, BTUser user, ControllerArgs args);
 
     /**
-     * Выполняет метод и возвращает ответ бота
+     * Выполняет метод и возвращает ответ
+     *
+     * Данный метод выполняется только после выполнения метода `Controller::test`!
+     * @see Controller
+     * @see Controller::test
      *
      * @param text - текст
      * @param user - пользователь
+     * @param args - аргументы контроллера
      * @return - ответ бота
      */
     protected abstract String execute(String text, BTUser user, ControllerArgs args);
