dc01ca717de363d26df5451a7d8dc45c92bb2f8f
Diego <msnimpu@gmail.com>
2020-02-05 13:12:46 +0300

    Контроллеры расписания на сегодня/завтра/сейчас

--- src/main/java/me/ling/kipfin/vkbot/controllers/timetable/TimetableNowController.java
+++ src/main/java/me/ling/kipfin/vkbot/controllers/timetable/TimetableNowController.java
@@ -7,39 +7,17 @@ import me.ling.kipfin.vkbot.commands.Controller;
 import me.ling.kipfin.vkbot.commands.ControllerArgs;
 import me.ling.kipfin.vkbot.controllers.TimetableController;
 import me.ling.kipfin.vkbot.entities.BTUser;
-import me.ling.kipfin.vkbot.messagemodels.SubjectsModel;
+import me.ling.kipfin.vkbot.messagemodels.TimetableNowModel;
 import me.ling.kipfin.vkbot.utils.BTUtils;
-import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.time.LocalTime;
+
 
 /**
- * Контроллер расписания на сегодня
+ * Отображает информацию о расписании в данный момент
  */
-public class TimetableDayController extends TimetableController {
-
-    /**
-     * Тест команды "Сегдня"
-     *
-     * @param args - аргументы
-     * @return - результат тестирования
-     */
-    protected boolean testToday(@NotNull ControllerArgs args) {
-        return args.test("Сегодня") ||
-                (args.test("Сегодня") && BTUtils.checkTextIsState(String.join(" ", args)));
-    }
-
-    /**
-     * Тест команды "Завтра"
-     *
-     * @param args - аргументы
-     * @return - результат тестирования
-     */
-    protected boolean testTomorrow(@NotNull ControllerArgs args) {
-        return args.test("Завтра") ||
-                (args.test("Завтра") && BTUtils.checkTextIsState(String.join(" ", args)));
-    }
+public class TimetableNowController extends TimetableController {
 
     /**
      * Тестирует контрллер
@@ -57,7 +35,8 @@ public class TimetableDayController extends TimetableController {
      */
     @Override
     public boolean test(String text, BTUser user, ControllerArgs args) {
-        return this.testToday(args) || this.testTomorrow(args);
+        return args.test("Сейчас") ||
+                (args.test("Сейчас") && BTUtils.checkTextIsState(String.join(" ", args)));
     }
 
     /**
@@ -75,10 +54,11 @@ public class TimetableDayController extends TimetableController {
     @Override
     public String execute(String text, BTUser user, ControllerArgs args) {
         String state = BTUtils.getStateFromStringOrUser(String.join(" ", args), user);
-        LocalDate date = this.testTomorrow(args) ? LocalDate.now().plus(1, ChronoUnit.DAYS) : LocalDate.now();
+        LocalTime time = LocalTime.now();
 
-        String dateString = DateUtils.toLocalDateString(date);
+        String dateString = DateUtils.toLocalDateString(LocalDate.now());
         TimetableMaster master = TimetableManager.downloadOrGetCache(dateString);
-        return SubjectsModel.getExtendedDay(state, user, master).toString();
+
+        return TimetableNowModel.getTimetableNowDayComponent(state, time, master).toString();
     }
 }
