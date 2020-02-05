387b4f46db02e20f93ce7f814352a465a5b110eb
Diego <msnimpu@gmail.com>
2020-02-05 13:13:44 +0300

    Дополнительная информация

---
+++ src/main/java/me/ling/kipfin/vkbot/controllers/AdditionalController.java
@@ -0,0 +1,20 @@
+package me.ling.kipfin.vkbot.controllers;
+
+import me.ling.kipfin.vkbot.commands.ControllerArgs;
+import me.ling.kipfin.vkbot.entities.BTUser;
+import me.ling.kipfin.vkbot.commands.Controller;
+import me.ling.kipfin.vkbot.entities.keboard.Keyboard;
+
+public class AdditionalController extends Controller {
+    @Override
+    public boolean test(String text, BTUser user, ControllerArgs args) {
+        return (user.isStudent() || user.isTeacher()) && this.testAlias(text, "Дополнительно");
+    }
+
+    @Override
+    public String execute(String text, BTUser user, ControllerArgs args) {
+        if(user.isStudent()) user.setKeyboard(Keyboard.additionalStudent);
+        if(user.isTeacher()) user.setKeyboard(Keyboard.additionalTeacher);
+        return "Дополнительная информация 👾";
+    }
+}
