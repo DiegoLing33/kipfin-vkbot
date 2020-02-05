7abcaad4fc2f31e848e5201251baa750df3c91b5
Diego <msnimpu@gmail.com>
2020-02-05 13:02:02 +0300

    Отображение версии

---
+++ src/main/java/me/ling/kipfin/vkbot/controllers/core/VersionController.java
@@ -0,0 +1,18 @@
+package me.ling.kipfin.vkbot.controllers.core;
+
+import me.ling.kipfin.vkbot.app.Application;
+import me.ling.kipfin.vkbot.commands.Controller;
+import me.ling.kipfin.vkbot.commands.ControllerArgs;
+import me.ling.kipfin.vkbot.entities.BTUser;
+
+public class VersionController extends Controller {
+    @Override
+    public boolean test(String text, BTUser user, ControllerArgs args) {
+        return args.test("/version", "версия", "/v");
+    }
+
+    @Override
+    protected String execute(String text, BTUser user, ControllerArgs args) {
+        return Application.Version;
+    }
+}
