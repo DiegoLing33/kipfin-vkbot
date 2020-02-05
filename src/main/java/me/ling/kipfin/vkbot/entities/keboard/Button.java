73936eb91118aaec3f025d830f1826133372ec30
Diego <msnimpu@gmail.com>
2020-02-05 12:53:45 +0300

    Улучшена клавиатура

--- src/main/java/me/ling/kipfin/vkbot/entities/keboard/Button.java
+++ src/main/java/me/ling/kipfin/vkbot/entities/keboard/Button.java
@@ -2,31 +2,65 @@ package me.ling.kipfin.vkbot.entities.keboard;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Кнопка на клавиатуре
+ */
 public class Button {
 
-    @JsonProperty
-    protected String color = "primary";
+    public static final String POSITIVE = "positive";
+    public static final String DEFAULT = "default";
+    public static final String NEGATIVE = "negative";
+    public static final String SECONDARY = "secondary";
 
+    /**
+     * Цвет кнопки
+     */
+    @JsonProperty
+    protected String color = POSITIVE;
+
+    /**
+     * Действие
+     */
     @JsonProperty
     protected KeyboardAction action;
 
-    public Button() {
-    }
+    /**
+     * Конструктор кнопки
+     */
+    public Button() { }
 
+    /**
+     * Конструктор кнопки
+     * @param action    - дейтсвие
+     * @param color     - цвет
+     */
     public Button(KeyboardAction action, String color) {
         this.color = color;
         this.action = action;
     }
 
+    /**
+     * Конструктор кнопки. Цвет при этом будет positive
+     * @param action    - действие
+     */
     public Button(KeyboardAction action) {
         this.action = action;
     }
 
+    /**
+     * Конструктор кнопки
+     * @param text  - текст кнопки
+     * @param color - цвет кнопки
+     */
     public Button(String text, String color){
         this.action = new KeyboardAction("text", text);
         this.color = color;
     }
 
+    /**
+     * Конструктор кнопки
+     * @param text  - текст кнопки
+     */
     public Button(String text){
         this.action = new KeyboardAction("text", text);
     }
--- src/main/java/me/ling/kipfin/vkbot/entities/keboard/Keyboard.java
+++ src/main/java/me/ling/kipfin/vkbot/entities/keboard/Keyboard.java
@@ -3,12 +3,57 @@ package me.ling.kipfin.vkbot.entities.keboard;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.ling.kipfin.vkbot.controllers.group.SelectGroupNextController;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Клавиатура
+ */
 public class Keyboard {
+
+    public static final Keyboard start =
+            new Keyboard()
+                    .add(new Button("Выбрать группу"));
+
+    public static final Keyboard student =
+            new Keyboard()
+                    .add(new Button("Сегодня"), 0)
+                    .add(new Button("Завтра"), 0)
+                    .add(new Button("Сейчас"), 1)
+                    .add(new Button("Дополнительно", Button.SECONDARY), 2);
+
+    public static final Keyboard teacher =
+            new Keyboard()
+                    .add(new Button("Сегодня"), 0)
+                    .add(new Button("Завтра"), 0)
+                    .add(new Button("Сейчас"), 1)
+                    .add(new Button("Дополнительно", Button.SECONDARY), 2);
+
+    public static final Keyboard courses =
+            new Keyboard()
+                    .add(new Button("1 курс"), 0)
+                    .add(new Button("2 курс"), 0)
+                    .add(new Button("3 курс"), 1)
+                    .add(new Button("4 курс"), 1)
+                    .add(new Button("Домой", Button.SECONDARY), 2);
+
+
+    public static final Keyboard additionalStudent =
+            new Keyboard()
+                    .add(new Button("Классный руководитель"), 0)
+                    .add(new Button("Неделя"), 1)
+                    .add(new Button("Выбрать группу", Button.SECONDARY), 2)
+                    .add(new Button("Справка", Button.SECONDARY), 2)
+                    .add(new Button("Домой", Button.SECONDARY), 3);
+
+    public static final Keyboard additionalTeacher =
+            new Keyboard()
+                    .add(new Button("Группа в руководстве"), 0)
+                    .add(new Button("Справка", Button.SECONDARY), 1)
+                    .add(new Button("Домой", Button.SECONDARY), 1);
 
     @JsonProperty("one_time")
     protected boolean oneTime = false;
