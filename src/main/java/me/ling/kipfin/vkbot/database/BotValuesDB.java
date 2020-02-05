99b21444b4aecb3c75e48f4ec5a4c17ca19936c3
Diego <msnimpu@gmail.com>
2020-02-05 12:54:05 +0300

    Подключенеи к базам данных

--- src/main/java/me/ling/kipfin/vkbot/database/BotValuesDB.java
+++ src/main/java/me/ling/kipfin/vkbot/database/BotValuesDB.java
@@ -1,12 +1,16 @@
 package me.ling.kipfin.vkbot.database;
 
 import me.ling.kipfin.core.EntityDB;
+import me.ling.kipfin.core.log.Logger;
+import me.ling.kipfin.core.managers.SQLManager;
 import me.ling.kipfin.core.sql.SQLObjectMapper;
 import me.ling.kipfin.exceptions.DatabaseEntityNotFoundException;
 import me.ling.kipfin.vkbot.entities.BotValue;
 import org.jetbrains.annotations.NotNull;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Map;
 
 public class BotValuesDB extends EntityDB<BotValue, DatabaseEntityNotFoundException> {
@@ -23,10 +27,11 @@ public class BotValuesDB extends EntityDB<BotValue, DatabaseEntityNotFoundExcept
 
     /**
      * Выполняет поиск значения бота
-     * @param type  - тип значения (категория)
-     * @param key   - ключ значения
-     * @return      - объект BotValue
-     * @throws DatabaseEntityNotFoundException  - исключение, если значение не найдено
+     *
+     * @param type - тип значения (категория)
+     * @param key  - ключ значения
+     * @return - объект BotValue
+     * @throws DatabaseEntityNotFoundException - исключение, если значение не найдено
      */
     @NotNull
     public BotValue getValue(String type, String key) throws DatabaseEntityNotFoundException {
@@ -34,5 +39,28 @@ public class BotValuesDB extends EntityDB<BotValue, DatabaseEntityNotFoundExcept
             if (botValue.getType().equals(type) && botValue.getKey().equals(key))
                 return botValue;
         throw new DatabaseEntityNotFoundException("Значение для бота " + type + "@" + key + " не найдено!");
+    }
+
+    /**
+     * Устанавливает значение
+     * @param type      - тип
+     * @param key       - ключ
+     * @param value     - значение
+     * @return          - результат
+     * @throws SQLException - исключения SQL
+     */
+    public boolean setValue(String type, String key, @NotNull Object value) throws SQLException {
+        var connection = SQLManager.getConnection();
+        Statement statement = connection.createStatement();
+        var updates = statement.executeUpdate(
+                String.format("UPDATE bot_values SET `value`='%s' WHERE `kw`='%s' AND `type`='%s'", value.toString(), key, type));
+        if (updates == 0) {
+             var result = statement.executeQuery(String.format("INSERT INTO bot_values (`kw`, `type`, `value`) VALUES ('%s', '%s', '%s')",
+                    key, type, value.toString())).rowInserted();
+             connection.close();
+             return result;
+        }
+        connection.close();
+        return true;
     }
 }
