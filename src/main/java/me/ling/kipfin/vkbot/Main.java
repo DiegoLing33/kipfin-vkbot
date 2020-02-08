24a5998fae169e302779c0877f56593da256abe6
diego <masnimpu@gmail.com>
2020-02-08 03:27:56 +0300

    Рефракторинг

--- src/main/java/me/ling/kipfin/vkbot/Main.java
+++ src/main/java/me/ling/kipfin/vkbot/Main.java
@@ -5,7 +5,6 @@ import me.ling.kipfin.core.Bootloader;
 import me.ling.kipfin.timetable.managers.TimetableManager;
 import me.ling.kipfin.vkbot.app.Application;
 
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Objects;
 
@@ -16,14 +15,19 @@ public class Main {
      *
      * @param args - аргументы
      */
-    public static void main(String[] args) throws SQLException, IOException {
+    public static void main(String[] args) throws SQLException {
+
+        // Загрузка стартовых данных. Библиотека kipfin.core
         Dotenv dotenv = Dotenv.load();
         Bootloader bootloader = new Bootloader(dotenv);
         bootloader.updateDatabase(false);
-        TimetableManager.TIMETABLE_WEB_PATH = dotenv.get("timetable_web_path");
+        TimetableManager.TIMETABLE_WEB_PATH = dotenv.get("timetable_web_path"); //fixme -- add bootloader modules
 
+        // Получение актуальных данных из .env
         Integer groupId = Integer.parseInt(Objects.requireNonNull(dotenv.get("vk.group_id")));
         String token = dotenv.get("vk.token");
+
+        // Созданеи и запуск приложения бота
         Application application = new Application(groupId, token);
         application.start();
     }
